d8d22211ffe46f21ffa8352c91fdb4a4dc4753ba
Pavel Beránek <pberanek91@Pavel-MacBook-Air.local>
2023-05-03 20:14:39 +0200

    pridano prijmeni

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 def main():
   jmeno = input("Zadej jmeno: ")
-  print("Ahoj", jmeno)  
+  prijmeni = input("Zadej prijmeni: ")
+  print("Ahoj", jmeno, prijmeni)
+
 
 if __name__ == "__main__":
   main()
+
+#git add .
+#git commit -m "Upravil jsem main."
+#git push
+
+#git config --global user.email "you@example.com"
+#git config --global user.name "Your Name"
+
+#git branch
+#git branch nova_featurka
+#git switch nova_featurka
+
