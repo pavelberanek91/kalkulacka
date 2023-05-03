a358653fa7139e4260eca8e54d3d0c0ae7dc8563
Pavel Beránek <pberanek91@Pavel-MacBook-Air.local>
2023-05-03 20:24:19 +0200

    uprava

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 def main():
   jmeno = input("Zadej jmeno: ")
-  print("Ahoj", jmeno)  
+  prijmeni = input("Zadej prijmeni: ")
+  print("Ahoj", jmeno, prijmeni)  
 
 if __name__ == "__main__":
   main()
+
+#git branch uprava
+#git switch uprava
