46aa37149db48454c0a6ee5d0abda16c3d0a20d8
Pavel Beránek <pberanek91@Pavel-MacBook-Air.local>
2023-05-03 20:32:03 +0200

    pridano nashle

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ def main():
   jmeno = input("Zadej jmeno: ")
   prijmeni = input("Zadej prijmeni: ")
   print("Ahoj", jmeno, prijmeni)  
+  print("tak nashle")
 
 if __name__ == "__main__":
   main()
