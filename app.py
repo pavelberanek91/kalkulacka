38eef82a321e3f90c95a4f755095fab6d6fba3d1
Pavel Beránek <pberanek91@Pavel-MacBook-Air.local>
2024-04-09 19:26:37 +0200

    pridal funkci na odecitani

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+def odecti(a: float, b: float) -> float:
+  return a - b
+
 def secti(a: float, b: float) -> float:
   return a + b
 
 def main():
   print(secti(a=5, b=3))
+
+# git add .
+# git commit -m "Pridal funkci na odecitani"
