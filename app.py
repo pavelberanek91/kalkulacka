91c53568689353f0da80cddc48832517e6ac905b
Pavel Beránek <pavelberanek91@gmail.com>
2025-04-22 17:16:39 +0000

    dodelal jsem modulo funkci kalkulacky

--- app.py
+++ app.py
@@ -10,4 +10,10 @@ def nasobeni(a, b):
 def deleni(a, b):
   return a / b
 
-def modulo(a, b): ... # tohle dodelej zdenku
+def modulo(a, b):
+  return a % b
+
+# git add .
+# git commit -m "dodelal jsem modulo funkci kalkulacky"
+# git push
+# git pull
